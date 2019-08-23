fb26296fc6dc22ef49ddec5f3c853d3cb31e9088
pankaj.vasudev <pankaj.vasudev@noishuhp3090.indiaebix.com>
2019-08-23 18:27:15 +0530

    First Commit from Ebix

---
+++ SeleniumWithJava/src/com/seleniumwithjava/test/SeleniumWithJavaGit.java
@@ -0,0 +1,12 @@
+package com.seleniumwithjava.test;
+
+public class SeleniumWithJavaGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Selenium with Java Git");
+		System.out.println("Came directly from Ebix");
+
+	}
+
+}
